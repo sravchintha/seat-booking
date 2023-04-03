290d6f478b367045afdf583fef8d37f7f7216547
Tarun Kumar Marineni <tarunmarineni@gmail.com>
2023-04-03 19:16:22 +0530

    Functions Of Seat Booking

--- clear_booking.js
+++ clear_booking.js
@@ -1,19 +1,38 @@
-//Clear Booking
+//4 CLEAR BOOKING
 function clear_booking(){
     console.log("4. CLEAR BOOKING")
     console.log("WARNING")
     rl.question('You Want To Clear BookingID? Enter : "Y"',(answer)=>{
         if(answer=='Y'){
+            //Clear BOOKING ID Data In BOOKING_DATA.TXT
             bookingData.forEach((data,index)=>{
-                bookingData[index].bookingid='_'
+                var i=index
+                bookingData[i].bookingid='_'
+                if(i<n){
+                    fs.appendFileSync(`booked_update.txt`,bookingData[i].seat+":"+bookingData[i].bookingid+";",err=>{
+                        if(err){
+                            console.log("Unable To Update Clear Booking")
+                        }
+                        else{
+                            console.log(bookingData[i].seat,":",bookingData[i].bookingid)
+                        }
+                    })
+                }
             })
+            //Clear BOOKING ID In USER.TXT
+            console.log('here::', userData)
             userData.forEach((line,index)=>{
-                userData[index].bookingid='_'
+                var i=index
+                userData[i].bookingid='_'
+                if(i<m){
+                    fs.appendFile(`user_update.txt`,userData[i].bookingid+":"+userData[i].email+";",(err)=>{
+                        if(err){
+                            console.log("Unable To Update Clear Booking")
+                        }
+                    })
+                }
+                menu()
             })
-            bookingData.forEach((data,index)=>{
-                console.log(bookingData[index].seat,bookingData[index].bookingid)
-            })
-            menu()
         }
         else{
             menu()
